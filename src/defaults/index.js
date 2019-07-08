ae22b48495e459167d492e686eba7f6f961682ef
Radz <aurhora.web@gmail.com>
2019-07-08 22:32:04 +0200

    Default reducer values

---
+++ src/defaults/index.js
@@ -0,0 +1,12 @@
+export default DEFAULTS = {
+    SIMULATION_PLAYER: {
+        framesInterval: 0,
+        frames: [],
+        totalDuration: 0,
+        currentlyPlayedSecond: 0,
+        FPS: 0,
+        simulationActive: false,
+        currentFrameIndex: 0,
+        currentlyPlayedFrames: []
+    }
+}
